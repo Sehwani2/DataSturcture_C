e774b2e5e7e162d5eda7191f853e80719095081c
unknown <hyn0216388@gmail.com>
2024-11-12 21:22:38 +0900

    Queue

---
+++ Queue/LinkedQueue/LinkedQueue.c
@@ -0,0 +1,63 @@
+#include "LinkedQueue.h"
+
+void  LQ_CreateQueue( LinkedQueue** Queue ){
+    (*Queue)    = (LinkedQueue*)malloc(sizeof(LinkedQueue));
+
+    (*Queue)->Count =0;
+    (*Queue)->Front = NULL;
+    (*Queue)->Rear = NULL;
+}
+
+void  LQ_DestroyQueue( LinkedQueue* Queue ){
+    while(!LQ_IsEmpty(Queue)){
+        Node* Popped = LQ_Dequeue(Queue);
+        LQ_DestroyNode(Popped);
+    }
+    free(Queue);
+}
+
+Node* LQ_CreateNode(char* Data){
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+    NewNode->Data = (char*)malloc(strlen(Data)+1);
+
+    strcpy(NewNode->Data,Data);
+
+    NewNode->NextNode = NULL;
+
+    return NewNode;
+}
+void  LQ_DestroyNode(Node* _Node ){
+    free(_Node->Data);
+    free(_Node);
+}
+
+void  LQ_Enqueue( LinkedQueue* Queue, Node* NewNode ){
+    if(Queue->Front == NULL){
+        Queue->Front = NewNode;
+        Queue->Rear = NewNode;
+        Queue->Count++;
+    }
+    else{
+        Queue->Rear->NextNode = NewNode;
+        Queue->Rear = NewNode;
+        Queue->Count++;
+    }
+}
+Node* LQ_Dequeue( LinkedQueue* Queue ){
+    Node* Front = Queue->Front;
+
+    if(Queue->Front->NextNode == NULL){
+        Queue->Front = NULL;
+        Queue->Rear = NULL;
+    }
+    else{
+        Queue->Front = Queue->Front->NextNode;
+    }
+    Queue->Count--;
+
+    return Front;
+}
+
+int   LQ_IsEmpty( LinkedQueue* Queue ){
+    return (Queue->Front == NULL);
+}
